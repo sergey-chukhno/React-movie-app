c5d397491714ef3ee8856f638f81b9e168cb7110
Sergey Chukhno <sergey.chukhno@laplateforme.io>
2025-04-27 15:59:39 +0200

    Created project structure

---
+++ react-movie-project/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <button onClick={() => setHasLiked(!hasLiked) }>
+        { hasLiked ? '❤️' : '🤍' }
+      </button>
+    </div>
+   
+  )
+}
+
+const App = () => {
+ 
+  return (
+    <div className = "card-container">
+      <Card title="Star Wars" rating={5} isCool={true} />
+      <Card title = "Avatar"/>
+      <Card title = "The Lion King"/>
+    </div>
+  )
+ }
+
+export default App
